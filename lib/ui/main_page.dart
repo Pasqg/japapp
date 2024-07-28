6405a7329dde83a11d33cb23b389dcbcf7cab995
Pasqg <pasquale.giovenale@gmail.com>
2024-07-29 00:27:59 +0200

    Add RandDataProvider
    Extract hiragana/katakana generation into a generic RandDataProvider that can be re-used for words/phrases as well.

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'dart:math';
+
+import 'package:japapp/core/rand_data_provider.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -19,164 +20,8 @@ class MainPageState extends State<MainPage>
   late TabController _tabController;
 
   String _selectedScript = "Hiragana";
-  String _currentKana = '';
+  (String, String) _currentKana = ('', '');
   bool _isNextHiraganaDisabled = true;
-
-  static const Map<String, String> KATAKANA_MAP = {
-    'ア': 'a',
-    'イ': 'i',
-    'ウ': 'u',
-    'エ': 'e',
-    'オ': 'o',
-    'カ': 'ka',
-    'キ': 'ki',
-    'ク': 'ku',
-    'ケ': 'ke',
-    'コ': 'ko',
-    'サ': 'sa',
-    'シ': 'shi',
-    'ス': 'su',
-    'セ': 'se',
-    'ソ': 'so',
-    'タ': 'ta',
-    'チ': 'chi',
-    'ツ': 'tsu',
-    'テ': 'te',
-    'ト': 'to',
-    'ナ': 'na',
-    'ニ': 'ni',
-    'ヌ': 'nu',
-    'ネ': 'ne',
-    'ノ': 'no',
-    'ハ': 'ha',
-    'ヒ': 'hi',
-    'フ': 'fu',
-    'ヘ': 'he',
-    'ホ': 'ho',
-    'マ': 'ma',
-    'ミ': 'mi',
-    'ム': 'mu',
-    'メ': 'me',
-    'モ': 'mo',
-    'ヤ': 'ya',
-    'ユ': 'yu',
-    'ヨ': 'yo',
-    'ラ': 'ra',
-    'リ': 'ri',
-    'ル': 'ru',
-    'レ': 're',
-    'ロ': 'ro',
-    'ワ': 'wa',
-    'ヲ': 'wo',
-    'ン': 'n',
-    'ガ': 'ga',
-    'ギ': 'gi',
-    'グ': 'gu',
-    'ゲ': 'ge',
-    'ゴ': 'go',
-    'ザ': 'za',
-    'ジ': 'ji',
-    'ズ': 'zu',
-    'ゼ': 'ze',
-    'ゾ': 'zo',
-    'ダ': 'da',
-    'ヂ': 'ji',
-    'ヅ': 'zu',
-    'デ': 'de',
-    'ド': 'do',
-    'バ': 'ba',
-    'ビ': 'bi',
-    'ブ': 'bu',
-    'ベ': 'be',
-    'ボ': 'bo',
-    'パ': 'pa',
-    'ピ': 'pi',
-    'プ': 'pu',
-    'ペ': 'pe',
-    'ポ': 'po',
-    'キャ': 'kya',
-    'キュ': 'kyu',
-    'キョ': 'kyo',
-    'シャ': 'sha',
-    'シュ': 'shu',
-    'ショ': 'sho',
-    'チャ': 'cha',
-    'チュ': 'chu',
-    'チョ': 'cho',
-    'ニャ': 'nya',
-    'ニュ': 'nyu',
-    'ニョ': 'nyo',
-    'ヒャ': 'hya',
-    'ヒュ': 'hyu',
-    'ヒョ': 'hyo',
-    'ミャ': 'mya',
-    'ミュ': 'myu',
-    'ミョ': 'myo',
-    'リャ': 'rya',
-    'リュ': 'ryu',
-    'リョ': 'ryo',
-    'ギャ': 'gya',
-    'ギュ': 'gyu',
-    'ギョ': 'gyo',
-    'ジャ': 'ja',
-    'ジュ': 'ju',
-    'ジョ': 'jo',
-    'ビャ': 'bya',
-    'ビュ': 'byu',
-    'ビョ': 'byo',
-    'ピャ': 'pya',
-    'ピュ': 'pyu',
-    'ピョ': 'pyo'
-  };
-
-  static const Map<String, String> HIRAGANA_MAP = {
-    'あ': 'a',
-    'い': 'i',
-    'う': 'u',
-    'え': 'e',
-    'お': 'o',
-    'か': 'ka',
-    'き': 'ki',
-    'く': 'ku',
-    'け': 'ke',
-    'こ': 'ko',
-    'さ': 'sa',
-    'し': 'shi',
-    'す': 'su',
-    'せ': 'se',
-    'そ': 'so',
-    'た': 'ta',
-    'ち': 'chi',
-    'つ': 'tsu',
-    'て': 'te',
-    'と': 'to',
-    'な': 'na',
-    'に': 'ni',
-    'ぬ': 'nu',
-    'ね': 'ne',
-    'の': 'no',
-    'は': 'ha',
-    'ひ': 'hi',
-    'ふ': 'fu',
-    'へ': 'he',
-    'ほ': 'ho',
-    'ま': 'ma',
-    'み': 'mi',
-    'む': 'mu',
-    'め': 'me',
-    'も': 'mo',
-    'や': 'ya',
-    'ゆ': 'yu',
-    'よ': 'yo',
-    'ら': 'ra',
-    'り': 'ri',
-    'る': 'ru',
-    'れ': 're',
-    'ろ': 'ro',
-    'わ': 'wa',
-    'を': 'wo',
-    'ん': 'n'
-  };
 
   @override
   void initState() {
@@ -194,15 +39,11 @@ class MainPageState extends State<MainPage>
   }
 
   void _nextKana() {
-    final random = Random();
-
     setState(() {
       if (_selectedScript == 'Hiragana') {
-        _currentKana =
-            HIRAGANA_MAP.keys.elementAt(random.nextInt(HIRAGANA_MAP.length));
+        _currentKana = RandDataProvider.HIRAGANA.get();
       } else {
-        _currentKana =
-            KATAKANA_MAP.keys.elementAt(random.nextInt(KATAKANA_MAP.length));
+        _currentKana = RandDataProvider.KATAKANA.get();
       }
     });
   }
@@ -216,13 +57,7 @@ class MainPageState extends State<MainPage>
   void _validateTransliteration() {
     String transliteration = _textEditingController.text.trim();
     _textEditingController.clear();
-    String? expected;
-    if (_selectedScript == 'Hiragana') {
-      expected = HIRAGANA_MAP[_currentKana];
-    } else {
-      expected = KATAKANA_MAP[_currentKana];
-    }
-
+    String expected = _currentKana.$2;
     if (expected == transliteration) {
       _showCustomSnackBar('Correct!', Colors.green);
     } else {
@@ -314,7 +149,7 @@ class MainPageState extends State<MainPage>
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  _currentKana,
+                  _currentKana.$1,
                   style: const TextStyle(
                       fontSize: 100, fontWeight: FontWeight.bold),
                 ),
