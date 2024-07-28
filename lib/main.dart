9607470fa000c083e8b5a104d69c20f4e1cafea9
Pasqg <pasquale.giovenale@gmail.com>
2024-07-28 12:59:27 +0200

    Add main page of the app
    The MainPage is a tab view, with three tabs:
    - Hiragana: practice transliterating hiraganas (perhaps also drawing them)
    - Words: dictionary learning and practice
    - Talk: chatGpt based conversational partner
    
    This commit adds a basic version of hiragana view,
    that draws a random hiragana, displays it and prompts the user for a transliteration. On pressing Enter or the button "next", the app validates the transliteration and displays a correct/incorrect overlay.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:japly/ui/main_page.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,44 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: 'Hiragana App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: ''),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: MainPage(),
     );
   }
 }
---
+++ lib/ui/main_page.dart
@@ -0,0 +1,192 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  MainPageState createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  final TextEditingController _textEditingController = TextEditingController();
+  String _currentHiragana = '';
+  bool _isNextHiraganaDisabled = true;
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final Map<String, String> _hiraganaMap = {
+    'あ': 'a',
+    'い': 'i',
+    'う': 'u',
+    'え': 'e',
+    'お': 'o',
+    'か': 'ka',
+    'き': 'ki',
+    'く': 'ku',
+    'け': 'ke',
+    'こ': 'ko',
+    'さ': 'sa',
+    'し': 'shi',
+    'す': 'su',
+    'せ': 'se',
+    'そ': 'so',
+    'た': 'ta',
+    'ち': 'chi',
+    'つ': 'tsu',
+    'て': 'te',
+    'と': 'to',
+    'な': 'na',
+    'に': 'ni',
+    'ぬ': 'nu',
+    'ね': 'ne',
+    'の': 'no',
+    'は': 'ha',
+    'ひ': 'hi',
+    'ふ': 'fu',
+    'へ': 'he',
+    'ほ': 'ho',
+    'ま': 'ma',
+    'み': 'mi',
+    'む': 'mu',
+    'め': 'me',
+    'も': 'mo',
+    'や': 'ya',
+    'ゆ': 'yu',
+    'よ': 'yo',
+    'ら': 'ra',
+    'り': 'ri',
+    'る': 'ru',
+    'れ': 're',
+    'ろ': 'ro',
+    'わ': 'wa',
+    'を': 'wo',
+    'ん': 'n'
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _textEditingController.addListener(_enableDisableNextButton);
+    _generateRandomHiragana();
+  }
+
+  void _generateRandomHiragana() {
+    final random = Random();
+    setState(() {
+      _currentHiragana =
+          _hiraganaMap.keys.elementAt(random.nextInt(_hiraganaMap.length));
+    });
+  }
+
+  void _enableDisableNextButton() {
+    setState(() {
+      _isNextHiraganaDisabled = _textEditingController.text.isEmpty;
+    });
+  }
+
+  void _validateTransliteration() {
+    String transliteration = _textEditingController.text.trim();
+    _textEditingController.clear();
+    if (_hiraganaMap[_currentHiragana] == transliteration) {
+      _showCustomSnackBar('Correct!', Colors.green);
+    } else {
+      _showCustomSnackBar('Incorrect!', Colors.red);
+    }
+    _enableDisableNextButton();
+    _generateRandomHiragana();
+  }
+
+  void _showCustomSnackBar(String message, Color color) {
+    OverlayState overlayState = Overlay.of(context);
+    OverlayEntry overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: MediaQuery.of(context).padding.top,
+        left: 0,
+        right: 0,
+        child: Material(
+          color: color,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              message,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+    overlayState.insert(overlayEntry);
+    Timer(const Duration(seconds: 2), () {
+      overlayEntry.remove();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        toolbarHeight: 0,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Hiragana'),
+            Tab(text: 'Words'),
+            Tab(text: 'Talk'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildHiraganaView(),
+          const Center(child: Text('Coming soon...')),
+          const Center(child: Text('Coming soon...')),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHiraganaView() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _currentHiragana,
+              style:
+                  const TextStyle(fontSize: 100, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Transliteration',
+              ),
+              onSubmitted: (value) {
+                if (!_isNextHiraganaDisabled) {
+                  _validateTransliteration();
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed:
+                  _isNextHiraganaDisabled ? null : _validateTransliteration,
+              child: const Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
